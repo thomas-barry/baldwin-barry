14e4ece524611320eb6a386ffae5c1c4be81cc8c
Thomas Barry <thomas@baldwinbarry.com>
2019-01-13 17:19:22 -0500

    add support for styled-components

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import SEO from '../components/seo'
 
 const SecondPage = () => (
@@ -9,7 +9,7 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Link to="/">Home</Link>
   </Layout>
 )
 
