837bfe316900a0b1f65dcc65c90477b5e5d19221
Thomas Barry <thomas@baldwinbarry.com>
2019-02-27 20:54:01 -0500

    add speech

---
+++ src/components/speech-recognition-demo/SpeechRecognitionDemo.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Button from '../button/Button'
+import useSpeechEngine from './use-speech-engine'
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;  
+`
+
+const SpeechRecognitionDemo = () => {
+
+  const {
+    speechEnabled,
+    listen,
+    listening,
+    speechResult,
+  } = useSpeechEngine() 
+
+  return (
+    <React.Fragment>
+      <p>Speech Recognition Enabled: {speechEnabled ? 'Yes' : 'No'}</p>
+      <p>Listening: {listening ? 'Yes' : 'No'}</p>
+      <p>Result: {speechResult}</p>
+      <ButtonContainer>
+        <Button onClick={listen}>Listen</Button>
+      </ButtonContainer>
+    </React.Fragment>
+  )
+
+}
+
+export default SpeechRecognitionDemo
---
+++ src/components/speech-recognition-demo/use-speech-engine.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+
+window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition
+
+const useSpeechEngine = () => {
+  const [speechEngine, setSpeechEngine] = useState(null)
+  const [speechResult, setSpeechResult] = useState()
+  const [listening, setListening] = useState(false)
+  
+  useEffect(() => {
+    if ('SpeechRecognition' in window) {
+      const speechEngine = new window.SpeechRecognition()
+      speechEngine.onresult = onSpeechResult(speechEngine)
+      speechEngine.onerror = onSpeechError(speechEngine)
+      speechEngine.continuous = true
+      setSpeechEngine(speechEngine)
+    }
+  }, [])
+
+  const listen = () => {
+    setListening(true)
+    setSpeechResult('')
+    speechEngine.start()
+  }
+
+  const onSpeechResult = speechEngine => event => {
+    setListening(false)
+    setSpeechResult(event.results[0][0].transcript)
+    speechEngine.stop()
+  }
+
+  const onSpeechError = speechEngine => event => {
+    console.log(event)
+    speechEngine.stop()
+  }
+
+  return ({
+    speechEnabled: speechEngine !== null,
+    listen,
+    listening,
+    speechResult,
+  })
+
+}
+
+export default useSpeechEngine
