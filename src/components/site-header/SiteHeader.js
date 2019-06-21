3c0644cd7ebc52dbb516682fcfd50a6b6a834131
Thomas Barry <thomas@baldwinbarry.com>
2019-06-21 19:58:17 -0400

    animate site header

--- src/components/site-header/SiteHeader.js
+++ src/components/site-header/SiteHeader.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import styled from '@emotion/styled'
 
-// import useScrollPosition from '../hooks/use-scroll-position'
+import useScrollPosition from '../hooks/use-scroll-position'
+
+const DOWN = 0
+const UP = 1
 
 const StyledOuterContainer = styled.div`
   position: fixed;
@@ -18,15 +21,18 @@ const StyledOuterContainer = styled.div`
 const StyledInnerContainer = styled.div`
   margin: 0 auto;
   max-width: 960px;
-  padding: .8rem 1.0875rem; 
+  padding: 0.8rem 1.0875rem;
+  padding: ${({ small }) => (small ? '0.2rem 1rem' : '0.8rem 1rem')};
+  transition: padding 0.25s ease;
 `
 
 const StyledHeader = styled.h1`
-  font-size: 24px;
   margin: 0;
   letter-spacing: 2px;
   font-weight: normal;
   color: #dadada;
+  font-size: ${({ small }) => (small ? '16px' : '24px')};
+  transition: font-size 0.25s ease;
 `
 
 const StyledLink = styled(Link)`
@@ -35,13 +41,31 @@ const StyledLink = styled(Link)`
 `
 
 const Header = ({ siteTitle }) => {
-  // const { y: scrollTop } = useScrollPosition()
+  const { y: scrollTop } = useScrollPosition()
+  const [lastScrollTop, setLastScrollTop] = useState(scrollTop)
+  const [lastDirection, setLastDirection] = useState(DOWN)
+  const [distance, setDistance] = useState(0)
+
+  useEffect(() => {
+    setLastScrollTop(scrollTop)
+    const direction = scrollTop > lastScrollTop ? DOWN : UP
+    setDistance(scrollTop - lastScrollTop)
+    if (direction !== lastDirection) {
+      setLastDirection(scrollTop > lastScrollTop ? DOWN : UP)
+      setDistance(0)
+    }
+  }, [scrollTop])
+
+  const small = scrollTop > 80
+
+  console.log('Distance', distance)
+
   return (
     <StyledOuterContainer>
-      <StyledInnerContainer>
-        <StyledHeader>
+      <StyledInnerContainer small={small}>
+        <StyledHeader small={small}>
           <StyledLink to="/">
-            {siteTitle}
+            {siteTitle} {lastDirection ? 'UP' : 'DOWN'}
           </StyledLink>
         </StyledHeader>
       </StyledInnerContainer>
